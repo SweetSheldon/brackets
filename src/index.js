0a9c2a7c3142185c577fd3a3fb6033262fb966a0
SweetSheldon <sheldongamesandlp@gmail.com>
2020-10-04 22:02:56 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var i,j,k;
+  var brackets =[];
+  for(i=0;i<bracketsConfig.length;i++){
+    brackets.push(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`)
+  }
+  for(i=0;i<str.length*2;i++){
+    for(j=0;j<str.length;j++){
+      for(k=0;k<brackets.length;k++){
+        str=str.replace(`${brackets[k]}`,'');
+      }
+    }
+  }
+  if(str==''){return true}
+  else{return false}
 }
